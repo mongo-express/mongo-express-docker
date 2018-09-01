8d3bdadd0f2ca0d2e4547f1e4a0e824613cf7c79
Nick Vissari <nvissari@hcpss.org>
2018-09-01 05:43:38 -0400

    Add support for docker swarm secrets

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -19,6 +19,41 @@ function wait_tcp_port {
     exec 6>&-
 }
 
+# from https://github.com/docker-library/mongo/blob/master/4.1/docker-entrypoint.sh
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+    local var="$1"
+    local fileVar="${var}_FILE"
+    local def="${2:-}"
+
+    if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+        echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+        exit 1
+    fi
+
+    local val="$def"
+
+    if [ "${!var:-}" ]; then
+        val="${!var}"
+    elif [ "${!fileVar:-}" ]; then
+        val="$(< "${!fileVar}")"
+    fi
+
+    export "$var"="$val"
+    unset "$fileVar"
+}
+
+# set env variables to content of the following {VAR}_FILE env variables
+file_env 'ME_CONFIG_BASICAUTH_USERNAME'
+file_env 'ME_CONFIG_BASICAUTH_PASSWORD'
+file_env 'ME_CONFIG_MONGODB_ADMINUSERNAME'
+file_env 'ME_CONFIG_MONGODB_ADMINPASSWORD'
+file_env 'ME_CONFIG_SITE_COOKIESECRET'
+file_env 'ME_CONFIG_SITE_SESSIONSECRET'
+
 # wait for the mongo server to be available
 echo Waiting for ${ME_CONFIG_MONGODB_SERVER}:${ME_CONFIG_MONGODB_PORT:-27017}...
 wait_tcp_port "${ME_CONFIG_MONGODB_SERVER}" "${ME_CONFIG_MONGODB_PORT:-27017}"
